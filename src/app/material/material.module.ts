ad33cca68e1cd3e8ebe09dbd20960de9c6b7b0f8
ruben <rubenx4p@gmail.com>
2021-02-04 23:18:45 +0200

    dockerized

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { MatSliderModule } from '@angular/material/slider';
 import { MatButtonModule } from "@angular/material/button";
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatIconModule} from '@angular/material/icon';
-import {MatInputModule} from '@angular/material/input';
-import {MatCardModule} from '@angular/material/card';
-import {MatGridListModule} from '@angular/material/grid-list';
-import {MatListModule} from '@angular/material/list';
-import {MatDividerModule} from '@angular/material/divider';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatListModule } from '@angular/material/list';
+import { MatDividerModule } from '@angular/material/divider';
+
+import { OverlayContainer } from '@angular/cdk/overlay';
 
 @NgModule({
   declarations: [],
@@ -23,4 +25,8 @@ import {MatDividerModule} from '@angular/material/divider';
     MatDividerModule
   ]
 })
-export class MaterialModule { }
+export class MaterialModule {
+  constructor(overlayContainer: OverlayContainer) {
+    overlayContainer.getContainerElement().classList.add('unicorn-dark-theme');
+  }
+}
